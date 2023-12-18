e334ad6eb5525310d92a85d7d420db516b60fee4
Yeekaiser <asura200576@gmail.com>
2023-12-19 01:02:48 +0800

    Added player and movement script

--- GMAPS Group 4 Project/Assets/Yikai/Scripts/Breakable.cs
+++ GMAPS Group 4 Project/Assets/Yikai/Scripts/Breakable.cs
@@ -1,18 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Breakable : MonoBehaviour
 {
+    [SerializeField]
+    GameObject IntactGlass;
+
+    [SerializeField]
+    GameObject BrokenGlass;
+
+    BoxCollider col;
     // Start is called before the first frame update
     void Start()
     {
-        
+        col = gameObject.GetComponent<BoxCollider>();
+
+        IntactGlass.SetActive(true);
+        BrokenGlass.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        Break();
+    }
+    // Update is called once per frame
+    public void Break()
+    {
+        col.enabled = false;
+
+        IntactGlass.SetActive(false);
+        BrokenGlass.SetActive(true);
     }
 }
